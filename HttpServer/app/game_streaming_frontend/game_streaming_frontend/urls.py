2db079baf1408399d83d104a7631780d64e12534
422404 <elyanpoujol@gmail.com>
2019-08-13 03:15:50 +0200

    Website in progress

--- HttpServer/app/game_streaming_frontend/game_streaming_frontend/urls.py
+++ HttpServer/app/game_streaming_frontend/game_streaming_frontend/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from game_streaming_frontend import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(template_name='game_streaming_frontend/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.index, name='index'),
 ]
--- HttpServer/run.sh
+++ HttpServer/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sudo docker run -d --rm --network game-streaming-net -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro -v $(pwd)/app:/opt/app:rw --name game-streaming-frontend http-server:1.0
+sudo docker run -d --rm --network game-streaming-net -v /etc/localtime:/etc/localtime:ro -v /etc/timezone:/etc/timezone:ro -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd)/app:/opt/app:rw --name game-streaming-frontend http-server:1.0
